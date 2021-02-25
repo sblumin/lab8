c8f37a734026e8fb202a02637a4dd385d01adcbc
Sara Blumin <sblumin@ucsd.edu>
2021-02-24 19:14:07 -0800

    thirdparty api lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'mcYBFMbaZEJTgBd9bEKpEHfCpLooFYBU';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.88, -117.236],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});
+
+	L.marker([32.88, -117.236], {
+          icon: L.mapquest.icons.marker(),
+          draggable: false
+        }).addTo(map);
 }
